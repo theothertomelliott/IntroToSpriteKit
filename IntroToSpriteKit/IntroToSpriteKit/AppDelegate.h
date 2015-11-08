d21d0c931df36b6b00738d46b609ea8ab2c2b172
Thomas Elliott <tom.w.elliott@gmail.com>
2015-11-07 19:44:58 -0500

    Add default SpriteKit project.

---
+++ IntroToSpriteKit/IntroToSpriteKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntroToSpriteKit
+//
+//  Created by Thomas Elliott on 11/7/15.
+//  Copyright © 2015 Tom Elliott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntroToSpriteKit/IntroToSpriteKit/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  IntroToSpriteKit
+//
+
+//  Copyright (c) 2015 Tom Elliott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
